98bb8a4ae69c8ed1c5081eada03a3f78c7e13891
Srijan Sood <srijansood@gmail.com>
2016-11-02 01:59:10 -0400

    Using terminal (negative) goal representation and stacked frames (instead of averaged)

--- game.py
+++ game.py
@@ -1,9 +1,9 @@
-import cv2
-
-import numpy as np
 import os
-import rewardFunction as rf
+import params
+import numpy as np
 from PIL import Image
+from collections import deque
+from sklearn.metrics import mean_squared_error
 from browser import get_game, screenshot, ActionChains
 
 """
@@ -22,9 +22,8 @@ class GameState:
     def __init__(self):
         self.steps = 0
         self.browser, self.game = get_game()
-        import ipdb; ipdb.set_trace()
-        self.state, self.score = self.screenshot()
-
+        self.states = deque(4*[self.screenshot()[0]], maxlen=4)    # stacked frames
+        self.goal = np.array(Image.open(params.goal).convert('L'))
 
     def frame_step(self, input_actions):
         """
@@ -37,83 +36,45 @@ class GameState:
         # actions are a vector with a bit corresponding to action to take
         # input_actions[0] == 1: do nothing
         # input_actions[1] == 1: flap the bird
-        if input_actions[1] == 1: # TODO: also serves as restart right now
+        if input_actions[1] == 1:   # Also serves as game restart
             ActionChains(self.browser).click().perform()
 
-        # exponential moving average of image frames
-        game_state_img, score = self.screenshot()
-        self.state = (1 - LAMBDA) * game_state_img + LAMBDA * self.state
-        new_state_img = Image.fromarray(np.uint8(self.state))
-        # new_state_ img.show()
-        game_state = self.state.copy()
+        game_state_img, game_state_img_grayscale = self.screenshot()
+        self.states.popleft()   # remove oldest frame
+        self.states.append(game_state_img)
 
-        terminal = False
+        reward = get_reward(game_state_img_grayscale, goal_img=self.goal)
+        terminal = self.steps == params.max_steps
 
-        # Template Matching for Task Representation
-        """
-        res = cv2.matchTemplate(bmp,goal,cv2.TM_CCORR_NORMED)
-        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-
-        top_left = max_loc
-        stateROI = bmp[top_left[1]:top_left[1] + w, top_left[0]:top_left[0] + h].copy()
-
-        goalWidth, goalHeight = stateROI.shape[:2]
-        goalROI = goal[0:goalWidth, 0:goalHeight].copy()
-
-        stateROI = cv2.cvtColor(stateROI, cv2.COLOR_BGR2GRAY)
-        goalROI = cv2.cvtColor(goalROI, cv2.COLOR_BGR2GRAY)
-
-        reward = 1 / (np.exp((rf.calculateDistance(stateROI, goalROI))))
-        """
-        # for f in os.listdir(GOAL_TEMPLATES):
-        for f in ['03.jpg']:
-            # game_state_img = cv2.imread("/Users/Srijan/Dev/Research/flappy-bird-bot/images/between.png")
-            output = game_state_img.copy()
-            game_state_img = cv2.cvtColor(game_state_img, cv2.COLOR_BGR2GRAY)
-            goal_img = cv2.imread(os.path.join(GOAL_TEMPLATES, f))
-            goal_img = cv2.cvtColor(goal_img, cv2.COLOR_BGR2GRAY)
-
-            res = cv2.matchTemplate(image=game_state_img, templ=goal_img, method=cv2.TM_CCOEFF_NORMED)
-            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-            top_left = max_loc
-            h, w = goal_img.shape
-            best_match = game_state_img[top_left[1]:top_left[1] + h, top_left[0]:top_left[0] + w].copy()
-            dist1 = rf.calculateDistance(best_match, goal_img)
-            dist2 = sum(sum(abs(goal_img - best_match)))
-            rew1 = float(1) / float(np.exp(dist1))
-            rew2 = float(1) / float(np.exp(dist2))
-
-            print("Hog: {} <> Reward: {}   |   SSD: {} <> Reward: {}".format(dist1, rew1, dist2, rew2))
-            # cv2.rectangle(output, top_left, (top_left[0] + w, top_left[1] + h), color)
-
-            # Image.fromarray(output).show()
-
-
-
-            # stateROI = game_state_img[top_left[1]:top_left[1] + w, top_left[0]:top_left[0] + h].copy()
-            #
-            # goalWidth, goalHeight = stateROI.shape[:2]
-            # goalROI = goal_img[0:goalWidth, 0:goalHeight].copy()
-            #
-            # stateROI = cv2.cvtColor(stateROI, cv2.COLOR_BGR2GRAY)
-            # goalROI = cv2.cvtColor(goalROI, cv2.COLOR_BGR2GRAY)
-
-
-
-            # reward = 1 / (np.exp((rf.calculateDistance(stateROI, goalROI))))
-
-        reward = rew1
-
-        return game_state.astype(np.uint8), reward, terminal, self.score
+        return np.asarray(self.states, dtype=np.uint8), reward, terminal, 0
 
     def screenshot(self):
-        img, score = screenshot(self.browser, self.game)
+        """
+        :return: (img, 80x80 img) - Grayscale Screenshot of Game
+        """
+        img = screenshot(self.browser, self.game, grayscale=False)
 
         # (1280, 960, 3)
-        return np.array(img), np.array(score)
+        return np.array(img), np.array(img.convert('L'))
 
         # # (1228800, 3)
         # return np.array(img.getdata())
 
         # # (960, 1280, 3)
         # return np.array(img.getdata()).reshape(img.size[0], img.size[1], 3)
+
+
+def get_reward(state_img, goal_img):
+    """
+    Options:
+        1. "Positive" Goal, use template matching on the state_img, return inverse of distance to goal_img
+        2. "Negative" Goal, detect if state_img matches the death screen, return negative reward if so
+    """
+    threshold = 3.0
+    dist = mean_squared_error(state_img, goal_img)
+    print dist
+    if dist < threshold:
+        # death
+        return -1
+    else:
+        return 0
